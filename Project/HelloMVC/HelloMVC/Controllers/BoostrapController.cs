90e87cbe5918c1b212a368db4e095c351f139241
= <=>
2020-03-06 18:16:49 +0700

    Learn Boostrap

---
+++ Project/HelloMVC/HelloMVC/Controllers/BoostrapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class BoostrapController : Controller
+    {
+        // GET: Boostrap
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
